5f42b03f01e50b83278772e6ef871074f610afad
Simon Emanuel Schmid <simon@schmid.io>
2017-05-23 19:13:35 +0200

    simple and stupid login mech

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var login = require('./routes/login');
 
 var app = express();
 
@@ -20,10 +22,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({ secret: '!!!INSECURE!!!' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/login.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.post('/', function(req, res, next) {
+  if (req.body.username === 'alice' && req.body.password === 'password') {
+    req.session.username = req.body.username;
+    res.redirect('/');
+  } else {
+    res.render('error', { message: `No user ${req.body.username}. Try "alice:password"` });
+  }
+});
+
+module.exports = router;
